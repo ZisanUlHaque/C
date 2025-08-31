e9564e53d8fac6e24b4d27e36693e7ecfa11e597
Zisan <mdzisanulhaque989@gmail.com>
2025-08-31 20:55:35 +0600

    problem 6

---
+++ challange.js
@@ -0,0 +1,10 @@
+/** Problem 06 : (Current Salary ) */
+
+var experience = 30;
+var startingSalary = 45000;
+
+
+var currentSalary = startingSalary * Math.pow(1.05, experience);
+
+
+console.log(currentSalary.toFixed(2));
